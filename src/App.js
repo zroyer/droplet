e8c3bd43e586735f75ec5a7da61b70168d933120
Zach Royer <zro@crowdnoise.com>
2018-01-14 12:18:52 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <p className="App-intro">
+          <h1 className="App-title">☕️</h1>
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
