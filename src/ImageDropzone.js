e4505da40ceb03269b9eb1a7041db3cf8ceb76a7
Zach Royer <zro@crowdnoise.com>
2018-01-15 12:13:27 -0600

    spinner update

--- src/ImageDropzone.js
+++ src/ImageDropzone.js
@@ -70,7 +70,7 @@ class ImageDropzone extends React.Component {
 
     if(this.state.isUploading) {
       message = (
-        <Spinner name='wave' fadeIn='none' color='#ffffff'/>
+        <Spinner name='pulse' fadeIn='none' color='#ffffff'/>
       )
     } else if (!this.state.isUploading && this.state.imgurUrl) {
       message = (
