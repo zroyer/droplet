76be553222b7fb33a7c49784f8a1963b5fcc8929
Zach Royer <zro@crowdnoise.com>
2018-01-14 21:11:10 -0600

    posts image

--- src/ImageDropzone.js
+++ src/ImageDropzone.js
@@ -1,18 +1,38 @@
 import React from 'react';
 import Dropzone from 'react-dropzone'
+import axios from 'axios'
 
 class ImageDropzone extends React.Component {
   constructor() {
     super()
-    this.state = { files: [] }
+    this.state = {
+      imgurUrl: '',
+    }
   }
 
   onDrop(files) {
-    this.setState({
-      files
-    });
+    var form = new FormData()
+    form.append('image', files[0])
+    form.append('name', files[0].name)
 
-    console.log(files)
+    const config = {
+      baseURL: 'https://api.imgur.com',
+      headers: {
+        'Authorization': 'Client-ID 37266d00d3ab6ea'
+      }
+    }
+    axios.post('/3/image', form, config)
+    .then((result) => {
+      console.log('image post success')
+      this.setState({
+        imgurUrl: result.data.data.link.replace('https://', '')
+      });
+      console.log(result)
+    })
+    .catch((error) => {
+      console.log('image post error')
+      console.log(error)
+    })
   }
 
   render() {
@@ -23,12 +43,8 @@ class ImageDropzone extends React.Component {
             <p>Try dropping some files here, or click to select files to upload.</p>
           </Dropzone>
         </div>
-        <h2>Dropped files</h2>
-
-          {
-            this.state.files.map(f => <p key={f.name}>{f.name} - {f.size} bytes</p>)
-          }
-
+        <h2>Last file uploaded:</h2>
+        <p><a href={this.state.imgurUrl}>{this.state.imgurUrl}</a></p>
       </div>
     );
   }
